aa936fcaaf738b7f822b8c4b1e0a493481490dc8
Fabiano <fabianochou13@gmail.com>
2026-04-19 15:22:05 -0300

    feat: Adicionado modulo de perfil com suporte a logout

---
+++ app/src/main/java/com/example/alimepatia/ui/perfil/PerfilFragment.kt
@@ -0,0 +1,43 @@
+﻿package com.example.alimepatia.ui.perfil
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.alimepatia.R
+import com.example.alimepatia.databinding.FragmentPerfilBinding
+
+class PerfilFragment : Fragment() {
+
+    private var _binding: FragmentPerfilBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPerfilBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            // Acao de logout, limpando a back stack e voltando para a tela de Login
+            findNavController().navigate(
+                R.id.loginFragment, null,
+                androidx.navigation.NavOptions.Builder()
+                    .setPopUpTo(R.id.dashboardFragment, true)
+                    .build()
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
